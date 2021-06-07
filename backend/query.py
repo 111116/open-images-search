34cf836d6da5f95692757a0933f3e5281b6a16fa
Sengxian <sengxian@live.com>
2021-06-07 15:51:54 +0800

    fix query formatting error

--- backend/query.py
+++ backend/query.py
@@ -61,7 +61,7 @@ def search():
     query = request.args.get("q", "").lower()
     if not query:
         return {"urllist": []}
-    return {"urllist": list(map(lambda x:dict(imageinfo[x]), classes[class_dict[query]][0:100]))}
+    return {"urllist": list(map(lambda x:dict(imageinfo[x]._asdict()), classes[class_dict[query]][0:100]))}
 
 
 @app.route("/prompt", methods=["GET"])
